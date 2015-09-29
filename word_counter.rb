9eb0d3697f673dd3605c4db8bcc0df3700096f4d
Speaslee <sophiapeaslee@gmail.com>
2015-09-29 10:44:09 -0400

    word counter

--- word_counter.rb
+++ word_counter.rb
@@ -1,8 +1,21 @@
 class WordCounter
   # Your code goes here
+  attr_accessor :word_length, :dictionary
+  def initialize word_length, where
+    word_length = 5
+    words= where
+  end
+
+
+  def count_of_words_matching_length
+    a = dictionary.select {|word| word.length? == wc}
+   a.count
+  end
+
 end
 
 dictionary = File.read("/usr/share/dict/words").split
+
   # => ["A", "a", "aa", "aal", "aalii", "aam", "Aani", "aardvark", ...]
 wc = WordCounter.new 5, dictionary
 
